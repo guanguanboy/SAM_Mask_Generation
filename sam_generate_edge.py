d491b896177e18118a0e04987a44418b33f212af
liguanlin <517445163@qq.com>
2023-10-30 11:57:16 +0800

    add generate edge srcipts

---
+++ sam_generate_edge.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+
+from automatic_mask_and_probability_generator import \
+    SamAutomaticMaskAndProbabilityGenerator
+
+#from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+
+
+def normalize_image(image):
+    # Normalize the image to the range [0, 1]
+    min_val = image.min()
+    max_val = image.max()
+    image = (image - min_val) / (max_val - min_val)
+
+    return image
+
+
+def main():
+    device = "cuda"
+    model_type = "vit_h"
+    sam_checkpoint = "./segment_anything/sam_vit_h_4b8939.pth"
+
+    sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+    sam.to(device=device)
+    generator = SamAutomaticMaskAndProbabilityGenerator(sam)
+
+    img_path = 'assets/fish.jpg'
+    image = cv2.imread(img_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    masks = generator.generate(image)
+
+    p_max = None
+    for mask in masks:
+        p = mask["prob"]
+        if p_max is None:
+            p_max = p
+        else:
+            p_max = np.maximum(p_max, p)
+
+    edges = normalize_image(p_max)
+    edge_detection = cv2.ximgproc.createStructuredEdgeDetection(
+        './model/model.yml.gz')
+    orimap = edge_detection.computeOrientation(edges)
+    edges = edge_detection.edgesNms(edges, orimap)
+
+    # make output directory
+    Path('./output/edge/example').mkdir(parents=True, exist_ok=True)
+    plt.imsave('./output/edge/example/edge.png', edges, cmap='binary')
+
+
+if __name__ == "__main__":
+    main()
